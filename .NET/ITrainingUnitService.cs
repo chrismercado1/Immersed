76594868576d39a149abbeea96d60416babad835
christopher Mercado <75757885+chrismercado1@users.noreply.github.com>
2022-11-23 16:44:02 -0800

    Add files via upload

---
+++ .NET/ITrainingUnitService.cs
@@ -0,0 +1,19 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain.TrainingUnits;
+using Sabio.Models.Requests.TrainingUnits;
+using System.Collections.Generic;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface ITrainingUnitService
+    {
+        TrainingUnit GetById(int id);
+        List<TrainingUnitPrimaryTrainer> GetByPrimaryTrainerId(int userId);
+        List<TrainingUnit> GetAll();
+
+        Paged<TrainingUnit> GetByOrgId(int pageIndex, int pageSize, int query, int organizationId);
+        int Add(TrainingUnitAddRequest model, int userId);
+        void Update(TrainingUnitUpdateRequest model, int userId);
+        void UpdateStatus(TrainingUnitBaseUpdateRequest model, int userId);
+    }
+}
